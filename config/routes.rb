d84d2eac00e930a991317e1fb9683144e7330ca7
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-01 18:55:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   resources :posts, only: [:index, :new, :update,:show]
+=======
+  resources :posts, only: [:index, :new, :update, :show ]
+>>>>>>> e0e48948f1820eae5dc359e0ee9eaa12aab51e69
 #get 'posts/:id', to: 'posts#show', as: 'post'
 get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
 #patch 'posts/:id', to: 'posts#update'
